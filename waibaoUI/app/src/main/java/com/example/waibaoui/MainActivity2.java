2750be5be1e3860cc5ea43a9ee7ff25537d966d6
zeronow <38280119+zeronow@users.noreply.github.com>
2019-05-17 15:00:54 +0800

    add

--- waibaoUI/app/src/main/java/com/example/waibaoui/MainActivity2.java
+++ waibaoUI/app/src/main/java/com/example/waibaoui/MainActivity2.java
@@ -1,12 +1,20 @@
 package com.example.waibaoui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
 
 public class MainActivity2 extends AppCompatActivity {
     private SearchView searchview;
+    private RelativeLayout relativelayout_1;
+    private RelativeLayout relativeLayout_2;
+    private RelativeLayout relativeLayout_3;
+    private RelativeLayout relativeLayout_4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +31,46 @@ public class MainActivity2 extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String newText) {
                 return false;
+            }
+        });
+
+        relativelayout_1 = (RelativeLayout)findViewById(R.id.relativelayout_1);
+        relativeLayout_2 = (RelativeLayout)findViewById(R.id.relativelayout_2);
+        relativeLayout_3 = (RelativeLayout)findViewById(R.id.relativelayout_3);
+        relativeLayout_4 = (RelativeLayout)findViewById(R.id.relativelayout_4);
+
+        relativelayout_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        relativeLayout_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        relativeLayout_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,SettingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        relativeLayout_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,SettingActivity.class);
+                startActivity(intent);
             }
         });
 
---
+++ waibaoUI/app/src/main/java/com/example/waibaoui/SettingActivity.java
@@ -0,0 +1,17 @@
+package com.example.waibaoui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settingpage);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);//返回
+        actionBar.setTitle("系统设置");
+}
+}
